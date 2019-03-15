23467a7755e69e316b79fae02bb4689ad7e98e42
Gabriel Pedrosa <gabriel.pedrosa@waycarbon.com>
2019-03-14 22:17:31 -0300

    Backbone set up, with Webpack, TypeScript and React

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  }
+};
