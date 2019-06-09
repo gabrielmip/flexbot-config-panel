5fe2c153c7ed84b9b2e66f9cab99830003a42484
Gabriel Pedrosa <gabriel@Gabriels-iMac.local>
2019-06-08 22:39:12 -0300

    New rules on ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,23 @@
 module.exports = {
+  env: {
+    browser: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    sourceType: "module"
+    sourceType: "module",
+    ecmaVersion: "6",
+    jsx: true
   },
   plugins: ['@typescript-eslint'],
+  rules: {
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'jsx-quotes': ['warn', 'prefer-single'],
+    'quotes': ['warn', 'single', { 'avoidEscape': true }]
+  }
 }
