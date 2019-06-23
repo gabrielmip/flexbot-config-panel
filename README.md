c25dea35c61dc3152eb57e94478808dd79f0c927
Gabriel Pedrosa <gabriel@Gabriels-iMac.local>
2019-06-23 09:51:07 -0300

    um README e um deploy.sh melhorzin

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# flexbot-config-panel
+# Flexbot config panel
+
+## What is this?
+The configuration panel for **@FlegexBot**, a version of Slackbot for Telegram.
+When users send the command `/config`, they receive a URL to access the
+configuration panel. There, they can set up customized automatic responses.
+
+## The stack
+- React + TypeScript
+- Webpack
+- Material-UI
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+SSH_HOST="personal-site"
+DEPLOY_PATH="~/flexbot-config-panel/config"
 
 npm run build
 build_return=$?
@@ -7,8 +9,8 @@ if [ $build_return -ne 0 ]; then
   exit
 fi
 
-echo "Syncing..."
-rsync -a dist/ personal-site:~/flexbot-config-panel/config --progress
+echo "Syncing for ${SSH_HOST}:${DEPLOY_PATH}..."
+rsync -a dist/ $SSH_HOST:$DEPLOY_PATH --progress
 sync_return=$?
 if [ $sync_return -ne 0 ]; then
   echo "SYNC FAILED."
